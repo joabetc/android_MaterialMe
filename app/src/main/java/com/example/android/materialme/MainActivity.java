fe8d4dc2e23ca2b3ffa8bdff0afa57a5a9434bf9
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-04 16:55:03 -0300

    Implementing drag and drop

--- app/src/main/java/com/example/android/materialme/MainActivity.java
+++ app/src/main/java/com/example/android/materialme/MainActivity.java
@@ -24,6 +24,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 /***
  * Main Activity for the Material Me app, a mock sports news application
@@ -58,12 +60,19 @@ public class MainActivity extends AppCompatActivity {
         initializeData();
 
         ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper
-                .SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+                .SimpleCallback(
+                        ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT |
+                            ItemTouchHelper.DOWN | ItemTouchHelper.UP,
+                        ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
             @Override
             public boolean onMove(RecyclerView recyclerView,
                                   RecyclerView.ViewHolder viewHolder,
                                   RecyclerView.ViewHolder target) {
-                return false;
+                int from = viewHolder.getAdapterPosition();
+                int to = target.getAdapterPosition();
+                Collections.swap(mSportsData, from, to);
+                mAdapter.notifyItemMoved(from, to);
+                return true;
             }
 
             @Override
